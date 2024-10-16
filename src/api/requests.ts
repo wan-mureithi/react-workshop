fed24f2954ff51d2860a6b0d97f0e7428ec6334a
wan.mureithi <estherdee27@gmail.com>
2024-10-16 21:42:57 +0000

    add API calls

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,7 +1,60 @@
-import axios from "axios"
-import { User } from "../types"
+import axios, { AxiosRequestConfig, Method } from 'axios'
+import { User, Post, Album, Photo, Todo } from "../types"
 
 
+const BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+export async function makeRequest<T>(
+  endpoint: string,
+  method: Method = 'GET',
+  data: any = null,
+  params: any = null,
+): Promise<T> {
+  const options: AxiosRequestConfig = {
+    url: `${BASE_URL}${endpoint}`,
+    method,
+    headers: { 'Content-Type': 'application/json' },
+    data,
+    params,
+  };
+
+  try {
+    const response = await axios(options);
+    return response.data;
+  } catch (error: any) {
+    console.error(`Error: ${error.message}`);
+    throw new Error(`Failed to ${method} ${endpoint} data: ${error.message}`);
+  }
+}
+
+export async function getUsers(): Promise<User[]> {
+    return await makeRequest<User[]>('/users', 'GET')
+  }
+  
+  // Fetch all posts
+  export async function getPosts(): Promise<Post[]> {
+    return await makeRequest<Post[]>('/posts', 'GET')
+  }
+  
+  // Fetch all comments
+  export async function getComments(): Promise<Comment[]> {
+    return await makeRequest<Comment[]>('/comments', 'GET')
+  }
+  
+  // Fetch all albums
+  export async function getAlbums(): Promise<Album[]> {
+    return await makeRequest<Album[]>('/albums', 'GET')
+  }
+  
+  // Fetch all photos
+  export async function getPhotos(): Promise<Photo[]> {
+    return await makeRequest<Photo[]>('/photos', 'GET')
+  }
+  
+  // Fetch all todos
+  export async function getTodos(): Promise<Todo[]> {
+    return await makeRequest<Todo[]>('/todos', 'GET')
+  }
 export const fetchRandomUsers = async (count: number = 10): Promise<User[]> => {
     try {
       const response = await axios.get(`https://randomuser.me/api/?results=${count}`)
@@ -10,4 +63,4 @@ export const fetchRandomUsers = async (count: number = 10): Promise<User[]> => {
       console.error('Error fetching users:', error)
       throw error;
     }
-  };
+  }
--- src/types.ts
+++ src/types.ts
@@ -1,28 +1,61 @@
-
-export interface User {
-    gender: string;
-    name: {
-      title: string;
-      first: string;
-      last: string;
-    };
-    location: {
-      street: {
-        number: number;
-        name: string;
-      };
-      city: string;
-      state: string;
-      country: string;
-      postcode: string;
-    };
-    email: string;
-    phone: string;
-    cell: string;
-    picture: {
-      large: string;
-      medium: string;
-      thumbnail: string;
-    };
-    nat: string;
+ export interface Address {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+        lat: string;
+        lng: string;
+      }
   }
+  
+  export interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: {
+        name: string;
+        catchPhrase: string;
+        bs: string;
+      };
+  }
+  
+export interface Post {
+        id: number,
+        title: string,
+        body: string,
+        userId: number
+      
+  }
+  export interface Album {
+    userId: number;
+    id: number;
+    title: string;
+  }
+
+  export interface Comment {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+  }
+  export interface Photo {
+    albumId: number;
+    id: number;
+    title: string;
+    url: string;
+    thumbnailUrl: string;
+  }
+
+  export interface Todo {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+  }
+  
