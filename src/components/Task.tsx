993ea949964d8fccb18bf26237dbb0098d792a86
wan.mureithi <estherdee27@gmail.com>
2024-10-21 23:11:09 +0300

    add task list

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
-interface Task {
+
+export interface TaskType {
   id: string
   title: string
   state: string
 }
 interface TaskProps {
-  task: Task
+  task: TaskType
   onArchiveTask: (id: string) => void
   onPinTask: (id: string) => void
 }
